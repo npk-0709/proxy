999a76cf985903b5fdc4be56c89563dc61a9ff42
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2021-08-12 18:09:27 +0700

    CHECK PROXY LIVE OR DIE

---
+++ check-proxy.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    try:
+        from proxy_checker import ProxyChecker
+    except:
+        import os
+        os.system("pip install proxy_checker")
+        from proxy_checker import ProxyChecker
+    list_proxy=['<ip>:<port>']
+    for proxy in list_proxy:
+        check=ProxyChecker().check_proxy(proxy=proxy)
+        print(proxy+" # "+check)
+        # if "False" -> proxy die 
+        #==========================================
+        # example proxy live 
+        # {
+        # "country": "United States",
+        # "country_code": "US",
+        # "protocols": [
+        #   "socks4",
+        #   "socks5"
+        #  ],
+        # "anonymity": "Elite",
+        # "timeout": 1649
+        # }
